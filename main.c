6cbbf20b04a43fed3129d9a59848bb122802e8fe
korgabo <99967224+korgabo@users.noreply.github.com>
2022-02-21 10:42:16 +0100

    Update main.c

--- main.c
+++ main.c
@@ -10,9 +10,9 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 
 
-
 int main()
 {
+    
     /*
          int szam;
    int osszeg = 0;
@@ -22,7 +22,6 @@ int main()
        osszeg+=szam;
    }
    printf("A megadott számok összege:%d",osszeg);
-
     */
     
     /*
@@ -56,11 +55,55 @@ int main()
    printf("Pozitiv számok: %d \n", pozitiv);
    printf("Negatív számok: %d \n", negativ);
    */
+   /*
    int n1;
    int n2;
-   printf("Adjon meg az első pozitív egész számot:");
-   do{
+   printf("Adjon meg az első pozitív egész számot:\n");
+   scanf("%d",&n1);
+   while(n1 <= 0){
        scanf("%d",&n1);
-   }while(n1 > 0);
+   }
+      printf("Adjon meg a második pozitív egész számot:\n");
+   scanf("%d",&n2);
+   while(n2 <= 0){
+       scanf("%d",&n2);
+   }
+   if(n1 > n2){
+       printf("\nn1 > n2");
+   }
+   else if(n1 < n2){
+       printf("\nn1 < n2");
+   }
+   else{
+       printf("\bn1=n2");
+   }
+   */
+   /*
+   int n1;
+   int szam = 1;
+   printf("Adjon meg egy pozitív egész számot:");
+   scanf("%d",&n1);
+   while(szam < n1){
+       if(szam % 4 == 0){
+           printf("\n%d",szam);
+       }
+       ++szam;
+   }
+   */
+   /*
+     int n;
+   int szam = 1;
+   printf("Adjon meg egy pozitív egész számot:");
+   scanf("%d",&n);
+   while(szam < n){
+       ++szam;
+   }
+   while(szam >= 1){
+       if(szam%2 ==1){
+           printf("\n%d",szam);
+       }
+       --szam;
+   }
+*/
     return 0;
 }
